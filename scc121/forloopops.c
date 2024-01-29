de3fbbad486793f30c57b09b646d13adc3a1787a
bek000han <bekuniversity@gmail.com>
2024-01-29 11:15:55 +0000

    add new files

---
+++ scc121/forloopops.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int n = 5;
+    int i;
+
+    for (i = 0; i < n; i++){
+        printf("scc121\n");
+    }
+    printf("\n");
+    for (i = 1; i < n; i++){
+        printf("scc121\n");
+    }
+    printf("\n");
+    for (i = 0; i <= n; i++){
+        printf("scc121\n");
+    }
+    printf("\n");
+    for (i = 0; i < n; i+=2){
+        printf("scc121\n");
+    }
+    printf("\n");
+
+    return 0;
+}
