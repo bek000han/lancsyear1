a8802750ac9c2ede521ce680e4709a56f184db08
bek000han <bekzatkhan.maulen@gmail.com>
2024-02-28 21:56:47 +0000

    insertion NOT DONE!!!

---
+++ scc121/sorting/InsertionSort.java
@@ -0,0 +1,17 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+        int [] arr = {5,7,1,9,2,3,4,8,0,6};
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.err.println("\nsorted:");
+        insertionSort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private static void insertionSort(int[] arr){
+        
+    }
+}
