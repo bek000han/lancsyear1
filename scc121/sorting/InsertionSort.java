c79037f56da4c0b2c91461e47745354b05ca3528
bek000han <bekzatkhan.maulen@gmail.com>
2024-02-29 18:57:41 +0000

    insertion sort

--- scc121/sorting/InsertionSort.java
+++ scc121/sorting/InsertionSort.java
@@ -12,6 +12,17 @@ public class InsertionSort {
     }
 
     private static void insertionSort(int[] arr){
-        
+        int temp;
+        int j;
+        int k;
+        for (int i = 1; i < arr.length; i++) {
+            temp = arr[i];
+            j = i - 1;
+            while(j >= 0 && arr[j] > temp){
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
     }
 }
